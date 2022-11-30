2b86d3ac68020423ec06bc7e59fe2cd4e5707994
Mikael DEROME <mderome@e2r8p7.clusters.42paris.fr>
2022-11-30 13:47:31 +0100

    check number of arguments and valid's port

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -2,30 +2,32 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <string>
+#include <iostream>
+#include <cstdlib>
 
 #define	BUF_SIZE 10000
 
 
 int	main(int ac, char **av)
 {
-	struct addrinfo 		hint;
-	struct addrinfo 		*result, *rp;
-	int						sfd, s;
-	struct sockaddr_storage peer_addr;
-	socklen_t				peer_addr_len;
-	ssize_t					nread;
-	char 					buff[BUF_SIZE];
+	// struct addrinfo 		hint;
+	// struct addrinfo 		*result, *rp;
+	// int						sfd, s;
+	// struct sockaddr_storage peer_addr;
+	// socklen_t				peer_addr_len;
+	// ssize_t					nread;
+	// char 					buff[BUF_SIZE];
 
 	if (ac != 3)
 	{
 		std::cerr << "Usage: %s port password.\n";
 		return -1;
 	}
-	if (static_cast<int>av[1] < 0)
+	if (std::atoi(av[1]) < 0)
 	{
 		std::cerr << "The port can't be inferior at 0\n";
 		return -2;
 	}
-	
+	(void)av;
 	return 0;
 }
