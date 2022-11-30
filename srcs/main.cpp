9c8dcdc8a2a87f2e5c053a5305ca4702cbc0b467
Mikael DEROME <mderome@e2r8p7.clusters.42paris.fr>
2022-11-30 13:41:58 +0100

    main test

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -3,9 +3,29 @@
 #include <netdb.h>
 #include <string>
 
-int	main()
+#define	BUF_SIZE 10000
+
+
+int	main(int ac, char **av)
 {
-	struct addrinfo hint;
-	struct addrinfo *result, *rp
+	struct addrinfo 		hint;
+	struct addrinfo 		*result, *rp;
+	int						sfd, s;
+	struct sockaddr_storage peer_addr;
+	socklen_t				peer_addr_len;
+	ssize_t					nread;
+	char 					buff[BUF_SIZE];
+
+	if (ac != 3)
+	{
+		std::cerr << "Usage: %s port password.\n";
+		return -1;
+	}
+	if (static_cast<int>av[1] < 0)
+	{
+		std::cerr << "The port can't be inferior at 0\n";
+		return -2;
+	}
 	
+	return 0;
 }
