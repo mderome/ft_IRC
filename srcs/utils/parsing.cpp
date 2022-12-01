51409f18c76eea3700dd1f31ad50f3942656c650
mderome <dr4icus59@gmail.com>
2022-12-01 14:41:42 +0100

    parsing nam done, now start parsing command

--- srcs/utils/parsing.cpp
+++ srcs/utils/parsing.cpp
@@ -10,9 +10,10 @@ parsing::~parsing()
 
 bool    parsing::valid_server_name(std::string server_name)
 {
+    size_t i = 0;
     if (server_name.size() > 63)
         return (false);
-    for (size_t i = 0; i < server_name.size(); i++)
+    for (; i < server_name.size(); i++)
     {
         if (server_name[i] == ' ' || server_name[i] == '\t' || server_name[i] == '\n' || server_name[i] == '\r' || server_name[i] == '\v' || server_name[i] == '\f')
             return (false);
@@ -22,11 +23,18 @@ bool    parsing::valid_server_name(std::string server_name)
 
 bool    parsing::valid_user_name(std::string user_name)
 {
-    if (user_name.size() > 9)
+    size_t i = 0;
+    size_t point = 0;
+    if (user_name.size() > 16)
         return (false);
-    for (size_t i = 0; i < user_name.size(); i++)
+    if (user_name[i] == '.')
     {
-        if (user_name[i] == ' ' || user_name[i] == '\t' || user_name[i] == '\n' || user_name[i] == '\r' || user_name[i] == '\v' || user_name[i] == '\f'
+        i++;
+        point++;
+    }
+    for (; i < user_name.size(); i++)
+    {
+        if ((user_name[i] == ' ' && point == 0) || user_name[i] == '\t' || user_name[i] == '\n' || user_name[i] == '\r' || user_name[i] == '\v' || user_name[i] == '\f'
         || user_name[i] == '!' || user_name[i] == '@' || user_name[i] == '#' || user_name[i] == '$' || user_name[i] == ',' || user_name[i] == '.' || user_name[i] == '?')
             return (false);
     }
@@ -35,7 +43,7 @@ bool    parsing::valid_user_name(std::string user_name)
 
 bool    parsing::valid_channel_name(std::string channel_name)
 {
-    if (channel_name.size() > 50)
+    if (channel_name.size() > 50 || (!is_local(channel_name) && !is_regular(channel_name)))
         return (false);
     for (size_t i = 0; i < channel_name.size(); i++)
     {
@@ -48,8 +56,8 @@ bool    parsing::valid_channel_name(std::string channel_name)
 bool    parsing::is_local(std::string server_name)
 {
     if (server_name.find('&') == std::string::npos)
-        return (true);
-    return (false);
+        return (false);
+    return (true);
 }
 
 bool    parsing::is_regular(std::string server_name)
--- srcs/utils/parsing.hpp
+++ srcs/utils/parsing.hpp
@@ -14,11 +14,16 @@ class parsing
         parsing();
         ~parsing();
 
+
+        //validty name of user and channel
         bool    valid_server_name(std::string server_name);
         bool    valid_user_name(std::string user_name);
         bool    valid_channel_name(std::string channel_name);
         bool    is_local(std::string server_name);
         bool    is_regular(std::string server_name);
+
+        //parsing command
+        
 
 };
 
