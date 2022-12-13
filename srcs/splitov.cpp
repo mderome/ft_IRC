60d2482362b8f5e7d18101a97cca7867a3daed39
NargusS <https://NargusS@github.com>
2022-12-13 13:31:36 +0100

    splitov

--- srcs/splitov.cpp
+++ srcs/splitov.cpp
@@ -7,10 +7,10 @@ std::vector<std::string>    splitov(std::string str, char delim)
     std::vector<std::string>    res;
     std::string                 tmp;
     for (size_t i = 0; i < str.size(); i++) {
-        if (str[i] == delim) {
+        if ((str[i] == delim || str[i] == '\0') && tmp.length() > 0) {
             res.push_back(tmp);
             tmp.clear();
-        } else
+        } else if (str[i] != delim)
             tmp += str[i];
     }
     res.push_back(tmp);
